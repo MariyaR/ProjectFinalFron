e4ea27c75aaba19db151ee5702f8e08b90799d11
Mariia <mrozhavskaya@gmail.com>
2022-10-22 13:47:51 +0200

    with all prepared pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { EditProfileComponent } from './edit-profile/edit-profile.component';
 import { LoginComponent } from './login/login.component';
+import { PanierComponent } from './panier/panier.component';
+import { RegisterComponent } from './register/register.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
-  {path: 'userProfile', component: UserProfileComponent}];
+  {path: 'userProfile', component: UserProfileComponent},
+  {path: 'panier', component: PanierComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'editProfile', component: EditProfileComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
+import { AboutComponent } from './about/about.component';
+import { PanierComponent } from './panier/panier.component';
+import { RegisterComponent } from './register/register.component';
+import { EditProfileComponent } from './edit-profile/edit-profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    UserProfileComponent
+    UserProfileComponent,
+    AboutComponent,
+    PanierComponent,
+    RegisterComponent,
+    EditProfileComponent
   ],
   imports: [
     BrowserModule,
