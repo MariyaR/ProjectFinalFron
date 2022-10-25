c87d3a1667128b2efdccffcfb5ecc3244d963393
Pierre <pierre.cordier.pc@gmail.com>
2022-10-25 11:00:33 +0200

    login non modal ok

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Client } from '../client';
+import { Utilisateur } from '../utilisateur';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  utilisateur:Utilisateur = new Utilisateur();
+  message:string;
+  erreur:boolean = false;
+
+  constructor(private http : HttpClient) { }
 
   ngOnInit(): void {
+  }
+
+  connexion()
+  {
+    const body = JSON.stringify(this.utilisateur)
+    this.http.post("http://localhost:8080/utilisateur/connexion",body,{
+      headers: new HttpHeaders({
+        "Content-Type": "application/json"
+      }), responseType: "json"
+    }).subscribe(response => {
+      this.message="Authentification valide"
+      sessionStorage.setItem("client",JSON.stringify(response));
+    },
+      err => {
+        this.erreur = true
+        this.message="Erreur d'authentification : " + err.error.message;
+      })
   }
 
 }
